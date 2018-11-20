96471894c85f6ad454f69f9d37a5f1440e04c7c2
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-11-20 16:56:23 +0300

    Update упражнение 2.1.py

--- упражнение 2.1.py
+++ упражнение 2.1.py
@@ -1,43 +1,18 @@
-number = float (input("Введите номер химического элемента: "))
-
-"""if Number == 3.0:
-    print (Number, "Li-Литий")
-elif Number == 25.0:
-    print (Number, "Mn-Магний")
-elif Number == 80.0:
-    print (Number, "Hg-Ртуть")
-elif Number == 17.0:
-    print (Number, "Cl- Хлор")"""
-    
-
-
-
-"""def Number(x):
-    if x == 3.0:
-        return "Li-Литий"
-    elif x == 25.0:
-        return "Mn-Магний"
-    elif x == 80.0:
-        return "Hg-Ртуть"
-    elif x == 17.00:
-        return "Cl-Хлор"
-    else:
-        return "не надо"
-
-print ("Название элемента:" , Number(number))"""
-
-
 def fun_fact(x):
-    if x == 5.0:
-        return "хлебушек"
-    elif x==14.00:
-        return "кефирчик"
-    elif x==40.00:
-        return "булочка"
-    elif x==777.00:
-        return "пивко"
+    if number == 3.0:
+        return "Li-Литий"
+    elif number == 17.00:
+        return "Cl=Хлор"
+    elif number == 25.00:
+        return "Mn-Магний"
+    elif number == 80.00:
+        return "Hg- Ртуть"
     else:
-        "иди домой"
+        return "Неверные исходные данные"
 
-print ("Название продукта: ", fun_fact(number))
+
+number = int(input("Введите номер химического элемента: "))
+print("Химический элемент: ", fun_fact(number))
+        
+
         
