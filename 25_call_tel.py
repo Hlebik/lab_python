3ed818fb34d3e70fbec9414b0e7971e532115ac5
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-11-20 18:42:44 +0300

    Update and rename упражнение 2.5.py to 25_call_tel.py

---
+++ 25_call_tel.py
@@ -0,0 +1,15 @@
+def price (kod,time):
+    if kod == 343:
+        return time * 15
+    elif kod == 381:
+        return time * 18
+    elif kod == 473:
+        return time * 13
+    elif kod == 485:
+        return time * 11
+    else:
+        return("Введите корректный код: ")
+
+kod = int(input("Введите код города: "))
+time = int(input("Введите длительность переговоров: "))
+print("Стоимость разговора составляет: ", price(kod,time))
