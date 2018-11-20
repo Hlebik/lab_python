97e13cf6c0ed91ab6263ca8cf02770586a1baa9d
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-11-20 19:33:07 +0300

    Rename 26_cinema_price.py to 26_cinema_price .py

---
+++ 26_cinema_price .py
@@ -0,0 +1,44 @@
+def full_price_1(movie, ticket_amount):
+    if movie == "Пятница":
+        if time == "12 часов":
+            return 250* ticket_amount
+        elif time == "16 часов":
+            return 350*ticket_amount
+        elif time == "20 часов":
+            return 450*ticket_amount
+    elif movie == "Чемпионы":
+        if time == "10 часов":
+            return 250* ticket_amount
+        elif time == "13 часов":
+            return 350*ticket_amount
+        elif time == "16 часов":
+            return 350*ticket_amount
+    elif movie == "Пернатая банда":
+        if time == "10 часов":
+            return 350*ticket_amount
+        elif time == "14 часов":
+            return 450*ticket_amount
+        elif time == "18 часов":
+            return 450*ticket_amount
+
+        
+tmp_price= full_price_1(movie, ticket_amount)
+
+def full_price_2(date, tmp_price, ticket_amount):
+    if date == "завтра":
+        if ticket_amount>=20:
+            return tmp_price*0.8*0.95
+        else:
+            return tmp_price*0.95
+    elif date == "сегодня":
+        if ticket_amount>=20:
+            return tmp_price*0.8
+        else:
+            return tmp_price
+  
+print ("Стоимость билетов составляет: ", full_price_2(date,tmp_price,ticket_amount))
+print ("Программа для подсчета стоимости билетов в кино.")
+movie = input ("Выберите фильм: ")
+date = input ("Выберите дату: ")
+time = input ("Выберите время: ")
+ticket_amount = int(input ("Укажите количество билетов: "))
