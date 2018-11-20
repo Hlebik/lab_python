33390c27f29603e0d33df901a886611cf8e787a2
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-11-20 16:22:35 +0300

    Update кино (другой вариант).py

--- кино (другой вариант).py
+++ кино (другой вариант).py
@@ -4,7 +4,7 @@ date = input ("Выберите дату: ")
 time = input ("Выберите время: ")
 ticket_amount = int(input ("Укажите количество билетов: "))
 
-def full_price(movie, ticket_amount):
+def full_price_1(movie, ticket_amount):
     if movie == "Пятница":
         if time == "12 часов":
             return 250* ticket_amount
@@ -27,4 +27,20 @@ def full_price(movie, ticket_amount):
         elif time == "18 часов":
             return 450*ticket_amount
 
+print ("Стоимость билетов составляет: ", full_price_1(movie, ticket_amount))
 
+
+def full_price_2(date, full_price_1,ticket_amount):
+    if date == "завтра":
+        if ticket_amount>=20:
+            return full_price_1*0.8*0.95
+    elif date == "сегодня":
+        if ticket_amount>=20:
+            return full_price_1*0.8
+    else:
+        if ticket_amount <=20:
+            return full_price_1
+
+print ("Стоимость билетов составляет: ", full_price_2(date,full_price_1,ticket_amount))
+
+        
