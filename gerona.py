ae9ad75c9f9cc29ba2292035729a95bcdc684c0f
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-12-19 20:17:19 +0300

    Упражнение 3.1

---
+++ gerona.py
@@ -0,0 +1,25 @@
+# geron.py
+
+def geron(a, b, c):
+    ''' Вычисляет площадь треугольника по формуле Герона:
+    >>> geron(3, 4, 5)
+    6.0
+    
+    >>> geron(7, 8, 9)
+    26.83
+    '''
+    # здесь нужно написать собственный код
+    from math import sqrt
+    p = (a+b+c)/2
+    s = sqrt(p*(p-a)*(p-b)*(p-c))
+    return round(s, 2)
+  
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    
+
+
+
+    
