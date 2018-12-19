3db2389bba373e4abd016bf57cf8476a3418e6db
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-12-19 20:11:53 +0300

    Упражнение 2.1

---
+++ table_function.py.py
@@ -0,0 +1,16 @@
+def fun_fact(number):
+    if number == 3.0:
+        return "Li-Литий"
+    elif number == 17.00:
+        return "Cl=Хлор"
+    elif number == 25.00:
+        return "Mn-Магний"
+    elif number == 80.00:
+        return "Hg- Ртуть"
+    else:
+        return "Неверные исходные данные"
+
+
+number = int(input("Введите номер химического элемента: "))
+print("Химический элемент: ", fun_fact(number))
+        
