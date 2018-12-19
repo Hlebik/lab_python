72eecc4bff9b7e6b584c6a40b4f4f3ebe65a2420
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-12-19 22:57:33 +0300

    Упражнение 8.1

---
+++ Упражнение 8.1.py
@@ -0,0 +1,17 @@
+import random
+nums =[random.ranint(-20,20) for _ in range(20)]
+
+sums = {}
+sum [nums[i]+ nums[i+1]]
+sum[sum()] = i
+
+
+
+for i in range(len(nums)-5):
+max(sum)
+индекс и сумма
+
+
+
+print(count)
+    
