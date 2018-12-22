546804b4c28332da05f5bcb5fef19c74c6761e09
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-12-22 13:17:34 +0300

    Update упражнение 3.2.py.py

--- упражнение 3.2.py.py
+++ упражнение 3.2.py.py
@@ -6,7 +6,6 @@ print ("Постарайтесь отгадать его за минимальн
 
 the_number = random.randint (1, 4)
 guess = int (input("Ваше предложение: "))
-tries = 1
 while guess != the_number:
     if guess > the_number:
         print ("Меньше...")
@@ -16,14 +15,4 @@ while guess != the_number:
    
 print ("Вам удалось отгадать число! Это и в самом деле ", the_number)
 
-
-
-
-the_number = random.randint (1, 4)
-guess = int (input("Ваше предложение: "))
-tries = 1
-if guess == the_number:
-   print("Угадал")
-else:
-    print("Не угадал")
     
