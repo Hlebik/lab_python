15ae68c3db60ccaa61705c61032ae4a02f1dca6e
Hlebik <45106626+Hlebik@users.noreply.github.com>
2018-12-22 14:06:07 +0300

    Update random_letter1.py

--- random_letter1.py
+++ random_letter1.py
@@ -3,33 +3,16 @@
 пользователя ее угадать."""
 
 
-"""import random
-
-fun = ['самовар','весна','лето']
-a=random.choice(fun)
-lst = list(a)
-b=random.choice(lst)
-i = a.index(b)
-lst[i]= "?"
-print("выводим на экран:", ''.join(lst))
-
-
-guess = str(input("Give it a try:"))
-if guess == b:
-    print("Damn boy u did well")
-else:
-    print("Oh no, boy u better study harder")"""
-    
-
 import random
 while True:
-    word_ran = {'dog':'собака', 'cat':'кошка', 'bird':'птица', 'tiger': 'тигр', 'sea lion' : 'морской котик'} 
+    word_ran = {'dog': 'собака', 'cat': 'кошка', 'bird': 'птица',
+                'tiger': 'тигр', 'sea lion': 'морской котик'}
     a = word_ran
     lst = list(a)
     b = random.choice(lst)
-    print (b)
+    print(b)
     guess = str(input("Угадай слово:"))
     if guess == word_ran[b]:
-        print ("Вы угадали слово")
+        print("Вы угадали слово")
     else:
-        print ("Попробуйте еще раз")
+        print("Попробуйте еще раз")
