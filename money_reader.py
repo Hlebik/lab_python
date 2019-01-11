2e294083f172f16a140d4c824a5f11e9fe3af641
Hlebik <45106626+Hlebik@users.noreply.github.com>
2019-01-11 20:02:03 +0300

    Update money_reader.py

--- money_reader.py
+++ money_reader.py
@@ -1,24 +1,27 @@
 #def money_reader():
 
 import csv
-import operator
 money = {}
-a=[]
 with open('opendata.csv', encoding='cp1251') as csvfile:
-    money_reader = csv.reader(csvfile, delimiter=',') # delimiter по умолчанию ','
+    money_reader = csv.reader(csvfile) # delimiter по умолчанию ','
     for row in money_reader:
-        if row[0] == 'Количество заявок на потребительские кредиты':
-            if '2017' in row[2]:
-                if row[1] not in money:
-                    money[row[1]] = [row[3]]
-                else:
-                   money[row[1]].append(row[3])
+        if row[0] == 'Количество заявок на потребительские кредиты' and '2017' in row[2]:
+            if row[1] not in money:
+               money[row[1]] = [row[3]]
+            else:
+               money[row[1]].append(row[3])
                    
-
-
-
 for key, value in money.items():
     money[key] = sum(map(int, value))
 money.pop('Россия')
-print(max(money.items(), key=operator.itemgetter(1)))
+
+reg = ""
+max_value = 0
+for key, value in money.items():
+    if value > max_value:
+        max_value = value
+        reg = key
+
+print(reg, max_value)
+
 
