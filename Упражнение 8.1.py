63072b2c36f4bdf659e1571dd18f926d8f9a1e88
Hlebik <45106626+Hlebik@users.noreply.github.com>
2019-01-13 23:12:32 +0300

    упражнение 8.1

--- Упражнение 8.1.py
+++ Упражнение 8.1.py
@@ -1,16 +1,17 @@
 import random
 
 nums = [random.randint(-20, 20) for i in range(20)]
+sums = []
+for i in range(len(nums)-5): 
+    sums.append(list(nums[i:i+5]))
 
+max_sums = []
+max_sums = sums[0]
 
-sums = {}
-
-for i in range(len(nums)-5):
-   
-    sums[i] = sum(nums[i:i+5])
-
-a = max(sums.values())
-
+for i in sums:
+    if sum(sums[i])>sum(max_sums):
+        max_sums=sums[i]
+    
 print(sums)
-print (a)
 
+        
